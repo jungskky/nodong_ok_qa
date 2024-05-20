a500de1351d210e78d94d0f0ee8261ace8ab1bd2
jungskky <38771771+jungskky@users.noreply.github.com>
2024-05-20 10:06:12 +0900

    Update app.py
    title 수정

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ iface = gr.ChatInterface(
     fn = querying,
     chatbot=gr.Chatbot(height=600),
     textbox=gr.Textbox(placeholder="질문을 입력해 주세요.", container=False, scale=7),
-    title="쿠팡FAQ [(주)유아이네트웍스 AI 챗봇]",
+    title="노동OK Q&A [(주)유아이네트웍스 AI 챗봇]",
     theme="soft",
-    examples=["유효기간 안에 사용하지 못하면 환불 받을 수 있을까요?",
-              "무통장입금을 하고 주문을 취소했는데 환불은 언제 되나요?",],
+    examples=["밤샘근무 다음날 대체휴무로 쉬게 되면 급여는 어떻게 되나요?",
+              "실업급여를 부정하게 받으면 어떤 벌을 받게 되나요?",],
 
     cache_examples=True,
     retry_btn="Retry",
